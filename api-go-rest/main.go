ae0dc230cba172bb450ea2b3c406c120a5ae30bb
vitormdutra <ntw1942@gmail.com>
2025-12-21 17:30:07 -0300

    feat(api): implemented api rest

---
+++ api-go-rest/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"api-go-rest/database"
+	"api-go-rest/models"
+	"api-go-rest/routes"
+	"fmt"
+)
+
+func main() {
+	models.Personalidades = []models.Personalidade{
+		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
+		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
+	}
+
+	database.ConectarComBancoDeDados()
+
+	fmt.Println("Init Repo")
+	routes.HandleRequest()
+}
---
+++ api-go-rest/routes/routes.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"api-go-rest/controllers"
+	"api-go-rest/middleware"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+func HandleRequest() {
+	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletarUmaPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditarUmaPersonalidade).Methods("Put")
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
+}
